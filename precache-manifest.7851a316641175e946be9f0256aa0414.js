041d7df6d140ea18f684c60b5196c2794b68ab46
sathish kumar <sathishds2428@gmail.com>
2020-07-28 15:24:09 +0530

    Updates

---
+++ precache-manifest.7851a316641175e946be9f0256aa0414.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab77c5305d49003fa04c9e8742ad15e5",
+    "url": "/moviesportal/index.html"
+  },
+  {
+    "revision": "f38ef108eabccba88461",
+    "url": "/moviesportal/static/css/main.a39d1840.chunk.css"
+  },
+  {
+    "revision": "9c88598a85961229ea6a",
+    "url": "/moviesportal/static/js/2.b21e0366.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesportal/static/js/2.b21e0366.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f38ef108eabccba88461",
+    "url": "/moviesportal/static/js/main.5c6b52ad.chunk.js"
+  },
+  {
+    "revision": "b7340272353522046472",
+    "url": "/moviesportal/static/js/runtime-main.9f70329d.js"
+  }
+]);
